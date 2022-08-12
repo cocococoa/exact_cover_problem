36aa9b0e14bdeb4238c6d7b5bb2628ad21a719f2
masaki.ono <aaikmnoos@gmail.com>
2022-08-12 11:23:03 +0900

    Improve langford pair visualization

--- langford.cpp
+++ langford.cpp
@@ -1,6 +1,7 @@
 #include "dancing_links.h"
 
-void LangfordPair(int size, bool show_result = false) {
+void LangfordPair(int size, bool show_general_result = false,
+                  bool show_specific_result = false) {
   const int num_items = 2 * size + size;
   auto option_list = std::vector<std::vector<int>>{};
   for (auto i = 1; i <= size; ++i) {
@@ -15,24 +16,44 @@ void LangfordPair(int size, bool show_result = false) {
   auto solver = ExactCoverProblemSolver(num_items, option_list);
 
   std::cout << "Find exact cover via daincing links" << std::endl;
-  solver.Solve(not show_result);
+  solver.Solve(not(show_general_result or show_specific_result));
+  std::cout << "Done" << std::endl;
 
   const auto num_solutions = solver.NumSolutions();
-  std::cout << "Num solutions: " << num_solutions / 2 << std::endl;
-  if (show_result) {
+  std::cout << "Num solutions: " << num_solutions << std::endl;
+  std::cout << "Num solutions without symmetry: " << num_solutions / 2
+            << std::endl;
+  if (show_general_result) {
+    // General
     for (auto i = 0; i < num_solutions; ++i)
       std::cout << solver.GetPrettySolution(i) << std::endl;
+  }
+  if (show_specific_result) {
+    // Specific
+    auto array = std::vector<int>(2 * size, 0);
+    for (auto i = 0; i < num_solutions; ++i) {
+      const auto sol = solver.GetSolution(i);
+      for (const auto oidx : sol) {
+        const auto& option = option_list[oidx];
+        const auto l = option[0];
+        const auto r = option[1];
+        const auto number = option[2] - 2 * size + 1;
+        array[l] = number;
+        array[r] = number;
+      }
+      std::cout << Tostr(array.begin(), array.end(), "") << std::endl;
+    }
   }
 }
 
 int main() {
-  LangfordPair(3, true);
+  LangfordPair(3, true, true);
   std::cout << "======================================" << std::endl;
-  LangfordPair(4, true);
+  LangfordPair(4, true, true);
   std::cout << "======================================" << std::endl;
-  LangfordPair(7, true);
+  LangfordPair(7, false, true);
   std::cout << "======================================" << std::endl;
-  LangfordPair(8);
+  LangfordPair(8, false, true);
   std::cout << "======================================" << std::endl;
   LangfordPair(11);
   std::cout << "======================================" << std::endl;
