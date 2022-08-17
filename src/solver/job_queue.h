cdfccba6e8b9f00a475a36f0eb209337c7a2954f
masaki.ono <aaikmnoos@gmail.com>
2022-08-18 01:30:57 +0900

    Add comments to job_queue

--- src/solver/job_queue.h
+++ src/solver/job_queue.h
@@ -11,7 +11,9 @@ template <typename InputType, typename OutputType, typename Function>
 class JobQueue {
  public:
   JobQueue(int i_num_threads, Function i_function)
-      : num_threads(i_num_threads), function(i_function) {}
+      : num_threads(i_num_threads), function(i_function) {
+    std::cout << "Num threads: " << num_threads << std::endl;
+  }
   void AddJob(InputType input) {
     const auto lock = std::unique_lock<std::mutex>(input_mtx_);
     num_jobs_++;
@@ -22,21 +24,25 @@ class JobQueue {
       thread_list_.emplace_back(std::thread(
           [](std::mutex* input_mtx, std::queue<InputType>* input_queue,
              std::mutex* output_mtx, std::queue<OutputType>* output_queue,
-             Function function, std::atomic<bool>* finish) {
+             Function function, std::atomic_bool* finish) {
             while (true) {
-              if (*finish) {
-                break;
-              }
+              if (*finish) break;
+
+              // Get input mtx
               auto input_lock =
                   std::unique_lock<std::mutex>(*input_mtx, std::try_to_lock);
               if (not input_lock.owns_lock()) continue;
+
+              // Get input
               if (input_queue->empty()) continue;
               const auto input = input_queue->front();
               input_queue->pop();
               input_lock.unlock();
 
+              // Main process
               const auto output = function(input);
 
+              // Get output mtx
               const auto output_lock =
                   std::unique_lock<std::mutex>(*output_mtx);
               output_queue->push(output);
@@ -64,7 +70,7 @@ class JobQueue {
   const Function function;
 
   int num_jobs_ = 0;
-  std::atomic<bool> finish_ = false;
+  std::atomic_bool finish_ = std::atomic_bool{false};
   std::vector<std::thread> thread_list_;
   std::mutex input_mtx_;
   std::queue<InputType> input_queue_;
