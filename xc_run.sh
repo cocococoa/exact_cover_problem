f607e31ca5feeb80a2b86909a8ecca154a70af92
masaki.ono <aaikmnoos@gmail.com>
2022-08-24 10:01:04 +0900

    Update scripts: add shikaku

--- xc_run.sh
+++ xc_run.sh
@@ -5,6 +5,7 @@ time ./build/apps/xc_langford 2>&1    | tee -a xc_log
 time ./build/apps/xc_latin 2>&1       | tee -a xc_log
 time ./build/apps/xc_nqueen 2>&1      | tee -a xc_log
 time ./build/apps/xc_pentominoes 2>&1 | tee -a xc_log
+time ./build/apps/xc_shikaku 2>&1     | tee -a xc_log
 time ./build/apps/xc_sudoku 2>&1      | tee -a xc_log
 
 echo "-----------------------------------------------------------" | tee -a xc_log
